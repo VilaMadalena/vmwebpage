e52e403400a87a9d9f438dab35f8eb81f0c3aa2e
Manuel Moyano <mnlmoyano@gmail.com>
2014-07-03 12:55:36 -0300

    up

---
+++ js/main.js
@@ -0,0 +1,344 @@
+/* ------  SMOOTH SCROLL ----- */
+/*global $:false*/
+/*jshint unused:vars */
+/*jshint unused: true */
+ 
+$(document).ready(function() {
+   "use strict";
+   $('a[href*=#]').bind('click', function(e) {
+	e.preventDefault(); //prevent the "normal" behaviour which would be a "hard" jump
+       
+	var target = $(this).attr("href"); //Get the target
+			
+	// perform animated scrolling by getting top-position of target-element and set it as scroll target
+	$('html, body').stop().animate({ scrollTop: $(target).offset().top}, 2000, function() 
+        {
+        location.hash = target;  //attach the hash (#jumptarget) to the pageurl
+	});
+			
+	return false;
+   });
+
+});
+
+
+/**
+* jQuery Inview Event Plugin
+* author Christopher Blum
+* - based on the idea of Remy Sharp, http://remysharp.com/2009/01/26/element-in-view-event-plugin/
+* - forked from http://github.com/zuk/jquery.inview/
+*/
+(function ($) {
+  var inviewObjects = {}, viewportSize, viewportOffset,
+      d = document, w = window, documentElement = d.documentElement, expando = $.expando, timer;
+
+  $.event.special.inview = {
+    add: function(data) {
+      inviewObjects[data.guid + "-" + this[expando]] = { data: data, $element: $(this) };
+
+      // Use setInterval in order to also make sure this captures elements within
+      // "overflow:scroll" elements or elements that appeared in the dom tree due to
+      // dom manipulation and reflow
+      // old: $(window).scroll(checkInView);
+      //
+      // By the way, iOS (iPad, iPhone, ...) seems to not execute, or at least delays
+      // intervals while the user scrolls. Therefore the inview event might fire a bit late there
+      //
+      // Don't waste cycles with an interval until we get at least one element that
+      // has bound to the inview event.
+      if (!timer && !$.isEmptyObject(inviewObjects)) {
+         timer = setInterval(checkInView, 250);
+      }
+    },
+
+    remove: function(data) {
+      try { delete inviewObjects[data.guid + "-" + this[expando]]; } catch(e) {}
+
+      // Clear interval when we no longer have any elements listening
+      if ($.isEmptyObject(inviewObjects)) {
+         clearInterval(timer);
+         timer = null;
+      }
+    }
+  };
+
+  function getViewportSize() {
+    var mode, domObject, size = { height: w.innerHeight, width: w.innerWidth };
+
+    // if this is correct then return it. iPad has compat Mode, so will
+    // go into check clientHeight/clientWidth (which has the wrong value).
+    if (!size.height) {
+      mode = d.compatMode;
+      if (mode || !$.support.boxModel) { // IE, Gecko
+        domObject = mode === 'CSS1Compat' ?
+          documentElement : // Standards
+          d.body; // Quirks
+        size = {
+          height: domObject.clientHeight,
+          width: domObject.clientWidth
+        };
+      }
+    }
+
+    return size;
+  }
+
+  function getViewportOffset() {
+    return {
+      top: w.pageYOffset || documentElement.scrollTop || d.body.scrollTop,
+      left: w.pageXOffset || documentElement.scrollLeft || d.body.scrollLeft
+    };
+  }
+
+  function checkInView() {
+    var $elements = $(), elementsLength, i = 0;
+
+    $.each(inviewObjects, function(i, inviewObject) {
+      var selector = inviewObject.data.selector,
+          $element = inviewObject.$element;
+      $elements = $elements.add(selector ? $element.find(selector) : $element);
+    });
+
+    elementsLength = $elements.length;
+    if (elementsLength) {
+      viewportSize = viewportSize || getViewportSize();
+      viewportOffset = viewportOffset || getViewportOffset();
+
+      for (; i<elementsLength; i++) {
+        // Ignore elements that are not in the DOM tree
+        if (!$.contains(documentElement, $elements[i])) {
+          continue;
+        }
+
+        var $element = $($elements[i]),
+            elementSize = { height: $element.height(), width: $element.width() },
+            elementOffset = $element.offset(),
+            inView = $element.data('inview'),
+            visiblePartX,
+            visiblePartY,
+            visiblePartsMerged;
+        
+        // Don't ask me why because I haven't figured out yet:
+        // viewportOffset and viewportSize are sometimes suddenly null in Firefox 5.
+        // Even though it sounds weird:
+        // It seems that the execution of this function is interferred by the onresize/onscroll event
+        // where viewportOffset and viewportSize are unset
+        if (!viewportOffset || !viewportSize) {
+          return;
+        }
+        
+        if (elementOffset.top + elementSize.height > viewportOffset.top &&
+            elementOffset.top < viewportOffset.top + viewportSize.height &&
+            elementOffset.left + elementSize.width > viewportOffset.left &&
+            elementOffset.left < viewportOffset.left + viewportSize.width) {
+          visiblePartX = (viewportOffset.left > elementOffset.left ?
+            'right' : (viewportOffset.left + viewportSize.width) < (elementOffset.left + elementSize.width) ?
+            'left' : 'both');
+          visiblePartY = (viewportOffset.top > elementOffset.top ?
+            'bottom' : (viewportOffset.top + viewportSize.height) < (elementOffset.top + elementSize.height) ?
+            'top' : 'both');
+          visiblePartsMerged = visiblePartX + "-" + visiblePartY;
+          if (!inView || inView !== visiblePartsMerged) {
+            $element.data('inview', visiblePartsMerged).trigger('inview', [true, visiblePartX, visiblePartY]);
+          }
+        } else if (inView) {
+          $element.data('inview', false).trigger('inview', [false]);
+        }
+      }
+    }
+  }
+
+  $(w).bind("scroll resize", function() {
+    viewportSize = viewportOffset = null;
+  });
+  
+  // IE < 9 scrolls to focused elements without firing the "scroll" event
+  if (!documentElement.addEventListener && documentElement.attachEvent) {
+    documentElement.attachEvent("onfocusin", function() {
+      viewportOffset = null;
+    });
+  }
+})($);
+
+
+/* -- jQuery Count.To -- */
+(function ($) {
+	$.fn.countTo = function (options) {
+		options = options || {};
+
+		return $(this).each(function () {
+			// set options for current element
+			var settings = $.extend({}, $.fn.countTo.defaults, {
+				from:            $(this).data('from'),
+				to:              $(this).data('to'),
+				speed:           $(this).data('speed'),
+				refreshInterval: $(this).data('refresh-interval'),
+				decimals:        $(this).data('decimals')
+			}, options);
+
+			// how many times to update the value, and how much to increment the value on each update
+			var loops = Math.ceil(settings.speed / settings.refreshInterval),
+				increment = (settings.to - settings.from) / loops;
+
+			// references & variables that will change with each update
+			var self = this,
+				$self = $(this),
+				loopCount = 0,
+				value = settings.from,
+				data = $self.data('countTo') || {};
+
+			$self.data('countTo', data);
+
+			// if an existing interval can be found, clear it first
+			if (data.interval) {
+				clearInterval(data.interval);
+			}
+			data.interval = setInterval(updateTimer, settings.refreshInterval);
+
+			// initialize the element with the starting value
+			render(value);
+
+			function updateTimer() {
+				value += increment;
+				loopCount++;
+
+				render(value);
+
+				if (typeof(settings.onUpdate) == 'function') {
+					settings.onUpdate.call(self, value);
+				}
+
+				if (loopCount >= loops) {
+					// remove the interval
+					$self.removeData('countTo');
+					clearInterval(data.interval);
+					value = settings.to;
+
+					if (typeof(settings.onComplete) == 'function') {
+						settings.onComplete.call(self, value);
+					}
+				}
+			}
+
+			function render(value) {
+				var formattedValue = settings.formatter.call(self, value, settings);
+				$self.text(formattedValue);
+			}
+		});
+	};
+
+	$.fn.countTo.defaults = {
+		from: 0,               // the number the element should start at
+		to: 0,                 // the number the element should end at
+		speed: 1000,           // how long it should take to count between the target numbers
+		refreshInterval: 100,  // how often the element should be updated
+		decimals: 0,           // the number of decimal places to show
+		formatter: formatter,  // handler for formatting the value before rendering
+		onUpdate: null,        // callback method for every time the element is updated
+		onComplete: null       // callback method for when the element finishes updating
+	};
+	
+	
+	function formatter(value, settings) {
+		return value.toFixed(settings.decimals);
+	}
+}($));
+
+
+
+/* -- Upcoming Events -- */
+           $(function() {
+                //caching
+				//next and prev buttons
+				var $cn_next	= $('#cn_next');
+				var $cn_prev	= $('#cn_prev');
+				//wrapper of the left items
+				var $cn_list  = $('#cn_list');
+				var $pages		= $cn_list.find('.cn_page');
+				//how many pages
+				var cnt_pages	= $pages.length;
+				//the default page is the first one
+				var page  = 1;
+				//list of news (left items)
+				var $items  = $cn_list.find('.cn_item');
+				//the current item being viewed (right side)
+				var $cn_preview = $('#cn_preview');
+				//index of the item being viewed. 
+				//the default is the first one
+				var current		= 1;
+				
+				/*
+				for each item we store its index relative to all the document.
+				we bind a click event that slides up or down the current item
+				and slides up or down the clicked one. 
+				Moving up or down will depend if the clicked item is after or
+				before the current one
+				*/
+				$items.each(function(i){
+					var $item = $(this);
+					$item.data('idx',i+1);
+					
+					$item.bind('click',function(){
+						var $this   = $(this);
+						$cn_list.find('.selected').removeClass('selected');
+						$this.addClass('selected');
+						var idx			= $(this).data('idx');
+						var $current  = $cn_preview.find('.cn_content:nth-child('+current+')');
+						var $next		= $cn_preview.find('.cn_content:nth-child('+idx+')');
+						
+						if(idx > current){
+							$current.stop().animate({'top':'-300px'},600,'easeOutBack',function(){
+								$(this).css({'top':'310px'});
+							});
+							$next.css({'top':'310px'}).stop().animate({'top':'5px'},600,'easeOutBack');
+						}
+						else if(idx < current){
+							$current.stop().animate({'top':'310px'},600,'easeOutBack',function(){
+								$(this).css({'top':'310px'});
+							});
+							$next.css({'top':'-300px'}).stop().animate({'top':'5px'},600,'easeOutBack');
+						}
+						current = idx;
+					});
+				});
+				
+				/*
+				shows next page if exists:
+				the next page fades in
+				also checks if the button should get disabled
+				*/
+				$cn_next.bind('click',function(e){
+					var $this = $(this);
+					$cn_prev.removeClass('disabled');
+					++page;
+					if(page == cnt_pages)
+						$this.addClass('disabled');
+					if(page > cnt_pages){ 
+						page = cnt_pages;
+						return;
+					}	
+					$pages.hide();
+					$cn_list.find('.cn_page:nth-child('+page+')').fadeIn();
+					e.preventDefault();
+				});
+				/*
+				shows previous page if exists:
+				the previous page fades in
+				also checks if the button should get disabled
+				*/
+				$cn_prev.bind('click',function(e){
+					var $this = $(this);
+					$cn_next.removeClass('disabled');
+					--page;
+					if(page == 1)
+						$this.addClass('disabled');
+					if(page < 1){ 
+						page = 1;
+						return;
+					}
+					$pages.hide();
+					$cn_list.find('.cn_page:nth-child('+page+')').fadeIn();
+					e.preventDefault();
+				});
+				
+});
